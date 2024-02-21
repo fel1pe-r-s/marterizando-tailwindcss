cf1f74b78d558501f38caa6336ada62235db28af
fel1pe-r-s <1felipersilva1@gmail.com>
2024-02-21 13:41:51 -0300

    auto-animate

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -3,11 +3,14 @@
 import { Trash2, UploadCloud } from "lucide-react";
 import { useFileInput } from "./Root";
 import { formatBytes } from "@/ultils/format-bytes";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export const FileList = () => {
   const { files } = useFileInput();
+
+  const [parent] = useAutoAnimate();
   return (
-    <div className="mt-4 space-y-3">
+    <div className="mt-4 space-y-3" ref={parent}>
       {files.map((file) => (
         <div
           className="group flex items-start gap-4 rounded-lg border border-zinc-200 p-4"
